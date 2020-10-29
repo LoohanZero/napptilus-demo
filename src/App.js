1d6c0592ee10e0f73e148f14ce0645e864020654
Lujan Sanchez <mlulasan@gmail.com>
2020-10-29 13:07:31 +0100

    subo archivos iniciales y definiciones globales de CSS en index.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "./components/primitive/Container";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" />
+          <Route exact path="/:id" />
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
