f1d92a9646582bc3bd002b3d8e939e1a743ff068
Lujan Sanchez <mlulasan@gmail.com>
2020-10-29 15:03:07 +0100

    creo componentes primitivos Heading e Input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Container from "../components/primitive/Container";
-
+import Heading from "../components/primitive/Heading";
+import Text from "../components/primitive/Text";
+import Input from "../components/primitive/Input";
 
 const Home = () => {
-  return <Container></Container>;
+  return (
+    <Container as="main">
+      <Input />
+      <Heading>Find your Oompa Loompa</Heading>
+      <Text>There are more than 100k</Text>
+    </Container>
+  );
 };
 
 export default Home;
