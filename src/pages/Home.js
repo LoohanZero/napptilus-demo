79cfb4ed81a47789d0cc491ca2cc7530dca59b2c
Lujan Sanchez <mlulasan@gmail.com>
2020-10-29 15:14:43 +0100

    agrego estilos básicos de home y cambio estilos de clase header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,25 @@
 import React from "react";
 import Container from "../components/primitive/Container";
 import Heading from "../components/primitive/Heading";
-import Text from "../components/primitive/Text";
+import Image from "../components/primitive/Image";
 import Input from "../components/primitive/Input";
+import Text from "../components/primitive/Text";
+
 import searchIcon from "../imgs/ic_search.png";
+
+import style from "../styles/pages/home.module.css";
 
 const Home = () => {
   return (
-    <Container as="main">
-      <Input>
-      
-      <Input/>
+    <Container as="main" className={style["home-container"]}>
+      <Container>
+        <Input placeholder="Search" />
+        <Image scr={searchIcon} />
+      </Container>
+
       <Heading>Find your Oompa Loompa</Heading>
       <Text>There are more than 100k</Text>
+      <Container></Container>
     </Container>
   );
 };
