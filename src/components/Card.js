4345d411730b4d7ba48ad554b0126f06de18106e
Lujan Sanchez <mlulasan@gmail.com>
2020-10-29 16:42:42 +0100

    agrego parte de estilos a imagen de card y agrego padding bottom a cards container de home

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,23 @@ import React from "react";
 import Container from "./primitive/Container";
 import Heading from "./primitive/Heading";
 import Image from "./primitive/Image";
+import Span from "./primitive/Span";
 import Text from "./primitive/Text";
+import style from "../styles/components/card.module.css";
 
 const Card = ({ src, firstName, lastName, gender, profession }) => {
   const oompaGender = gender === "F" ? "Female" : "Male";
   return (
-    <Container as="article">
-      <Image src={src} />
-      <Heading level={2}>
-        {firstName}
-        {lastName}
+    <Container as="article" className={style["card-container"]}>
+      <Container className={style["image-container"]}>
+        <Image src={src} className={style["card-image"]} />
+      </Container>
+      <Heading level={2} className={style}>
+        <Span>{firstName}</Span>
+        <Span> {lastName}</Span>
       </Heading>
-      <Text>{oompaGender}</Text>
-      <Text>{profession}</Text>
+      <Text className={style}>{oompaGender}</Text>
+      <Text className={style}>{profession}</Text>
     </Container>
   );
 };
